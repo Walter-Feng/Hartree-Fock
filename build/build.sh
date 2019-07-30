ff6d7953b8aa15680b96418b468fedddb9c6a95c
walterfeng <walterfeng@foxmail.com>
2019-07-31 03:47:17 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,8 @@
-g++ -c ../src/*.cpp -g
-g++ *.o -o ../HF
+GSL_HOME=/usr/local
+export PATH=${GSL_HOME}/bin:$PATH
+export LD_LIBRARY_PATH=${GSL_HOME}/lib:$LD_LIBRARY_PATH
+export INCLUDE=${GSL_HOME}/include:$INCLUDE
+export MANPATH=${GSL_HOME}/share/man:$MANPATH
+
+g++ -c ../src/*.cpp -I${GSL_HOME}/include -g
+g++ *.o -static -L${GSL_HOME}/lib -lgsl -lgslcblas -lm -o ../HF
